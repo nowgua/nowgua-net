6292794fe3d2ad5d202c4ddafdc9bbd761365191
Krief David <d.krief@live.fr>
2018-02-15 14:24:46 +0100

    Update SiteAccessInformation.cs

--- nowguaClient/Models/Sites/SiteAccessInformation.cs
+++ nowguaClient/Models/Sites/SiteAccessInformation.cs
@@ -8,15 +8,15 @@ namespace nowguaClient.Models.Sites
     /// </summary>
     public class SiteAccessInformation 
     {
-        public List<BaseEntityLabel<int>> Type { get; set; }
+        public List<LabelModel<int>> Type { get; set; }
         public string Code { get; set; }
 
-        public Address Address { get; set; }
+        public AddressModel Address { get; set; }
         public string Commentaire { get; set; }
-        public BaseEntityLabel<int> LocationType { get; set; }
+        public LabelModel<int> LocationType { get; set; }
         public string KeyRef { get; set; }
 
-        public SiteAccessInformation(List<BaseEntityLabel<int>> Type,string Code, Address Address, string Commentaire,BaseEntityLabel<int> LocationType,string KeyRef )
+        public SiteAccessInformation(List<LabelModel<int>> Type,string Code, AddressModel Address, string Commentaire,LabelModel<int> LocationType,string KeyRef )
         {
             this.Type = Type;
             this.Code = Code;
