eb8cf6c3ecfae8927360b161c7e1b8ac99d3fc04
Chalom <shalom.murciano@gmail.com>
2019-02-24 17:41:18 +0200

    ajout de companyModel et CompanyService

---
+++ nowguaClient/Models/Compagnies/CompanyModel.cs
@@ -0,0 +1,33 @@
+﻿using nowguaClient.Models.Sites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nowguaClient.Models.Compagnies
+{
+    public class CompanyModel
+    {
+		public string Id { get; set; }
+		public string Name { get; set; }
+		public string Siren { get; set; }
+		public string Siret { get; set; }
+		public string Phone1 { get; set; }
+		public string Phone2 { get; set; }
+		public string Email { get; set; }
+		public string FileLogoCompany { get; set; }
+		public string Commentaire { get; set; }
+		public string CardId { get; set; }
+		public bool AgentStatusAvailabilityControl { get; set; }
+		public string CalendarId { get; set; }
+
+		public AddressModel Address { get; set; }
+
+		public bool HideContactSite { get; set; }
+
+		public bool Disabled { get; set; }
+
+		public bool Deleted { get; set; }
+
+		public long DeletedDate { get; set; }
+	}
+}
---
+++ nowguaClient/Services/CompanyService.cs
@@ -0,0 +1,62 @@
+﻿using Nest;
+using nowguaClient.Helpers;
+using nowguaClient.Models.Compagnies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nowguaClient.Services
+{
+	public interface ICompanyService : IBaseService
+	{
+		Task<List<CompanyModel>> SearchName(string Name);
+		Task<List<CompanyModel>> Search(Func<SearchDescriptor<CompanyModel>, ISearchRequest> selector);
+	}
+
+	/// <summary>
+	/// Gestion des sites 
+	/// </summary>
+	public class CompanyService : BaseService<CompanyModel>, ICompanyService
+	{
+		public CompanyService(IApiService ApiService, ISearchService SearchService)
+			: base(ApiService, SearchService, "/api/1.0/companies")
+		{
+
+		}
+
+		/// <summary>
+		/// Recherche d'une societe
+		/// </summary>
+		/// <param name="selector">Query ElasticSearch</param>
+		/// <returns>Liste des societes correspondant à la recherche</returns>
+
+		public Task<List<CompanyModel>> SearchName(string Name)
+		{
+			List<CompanyModel> companies = new List<CompanyModel>();
+
+			var must = new List<Func<QueryContainerDescriptor<CompanyModel>, QueryContainer>>();
+
+			must.Add(m => m.Term(new Field("name"), Name));
+			must.Add(m => m.Term(t => t.Field(f => f.Deleted).Value(false)));
+
+			companies = _searchService.Search<CompanyModel>(s => s.Type(SearchTypeName)
+																		.Query(q => q
+																			.Bool(b => b
+																				.Must(must)))
+																		.Size(100)
+																	);
+
+			return Task.FromResult(companies);
+		}
+
+		public Task<List<CompanyModel>> Search(Func<SearchDescriptor<CompanyModel>, ISearchRequest> selector)
+		{
+			List<CompanyModel> companies = new List<CompanyModel>();
+
+			companies = _searchService.Search<CompanyModel>(selector);
+
+			return Task.FromResult(companies);
+		}
+	}
+}
